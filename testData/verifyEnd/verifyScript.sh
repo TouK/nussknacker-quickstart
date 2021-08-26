e1e978426fd741fcd2fb1da8cdf5a9e43b7f83a5
apanczyk <apanczyk98@gmail.com>
2021-08-26 10:58:36 +0200

    Rename

--- testData/verifyEnd/verifyScript.sh
+++ testData/verifyEnd/verifyScript.sh
@@ -5,6 +5,6 @@
 ./specificSendTransactions.sh "$1" &
 
 if (docker exec -i nussknacker_kafka kafka-console-consumer.sh --topic alerts --bootstrap-server localhost:9092 &) | grep -q "Last packet"; then
-  printf "\nVerification done\n"
+  printf "\nLast request had been prepared\n"
   exit 0
 fi
