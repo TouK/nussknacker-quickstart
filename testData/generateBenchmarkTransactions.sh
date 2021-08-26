62d59630b885ff207849c95c3ebf16bf5e62ea7c
Maciej Brzeziński <mab816813@gmail.com>
2021-08-26 10:49:19 +0200

    fixes

--- testData/generateBenchmarkTransactions.sh
+++ testData/generateBenchmarkTransactions.sh
@@ -4,11 +4,18 @@ set -e
 
 cd "$(dirname $0)"
 
-while [ true ]; do
-  sleep 0.1
-  ID=$((1 + RANDOM % 5))
+CLIENTID_COUNT=${1:-100}
+TRANSACTIONS_PER_CLIENT=${2:-10}
+TRANSACTION_COUNT=$((CLIENTID_COUNT * TRANSACTIONS_PER_CLIENT))
+TIME_DEVIATION_PARAMETER=${3:-10}
+WINDOW_LENGTH=2000 #still don't know window length so using completely arbitrary value
+
+for ((i=1;i<=TRANSACTION_COUNT;i++));
+do
+  ID=$((1 + RANDOM % CLIENTID_COUNT))
   AMOUNT=$((1 + RANDOM % 30))
   NOW=`date +%s%3N`
-  TIME=$((NOW - RANDOM % 20))
+  TIME=$((NOW - RANDOM % (WINDOW_LENGTH*TIME_DEVIATION_PARAMETER)))
   echo "{ \"clientId\": \"$ID\", \"amount\": $AMOUNT, \"eventDate\": $TIME}"
-done | ./sendToKafka.sh transactions
+done > benchmarkTransactions.json
+
--- testData/setupAndSendBenchmarkTransactions.sh
+++ testData/setupAndSendBenchmarkTransactions.sh
@@ -7,36 +7,6 @@ cd "$(dirname $0)"
 CONTAINER_NAME=nussknacker_kafka
 TOPIC=$1
 PARTITION_COUNT=$2
-
-
-docker exec $CONTAINER_NAME kafka-topics.sh --create --topic "$TOPIC" --zookeeper zookeeper:2181 --partitions "$PARTITION_COUNT" --replication-factor 1 --if-not-exists
-cat ./benchmarkTransactions.json | ./sendToKafka.sh transactions
-
-
-
-
-
-
-#!/bin/bash
-
-set -e
-
-cd "$(dirname $0)"
-
-
-echo "Starting docker containers.."
-
-docker-compose -f docker-compose.yml -f docker-compose-env.yml kill
-docker-compose -f docker-compose.yml -f docker-compose-env.yml rm -f -v
-docker-compose -f docker-compose.yml -f docker-compose-env.yml build
-docker-compose -f docker-compose.yml -f docker-compose-env.yml up -d --no-recreate
-
-trap 'docker-compose -f docker-compose.yml -f docker-compose-env.yml kill && docker-compose -f docker-compose.yml -f docker-compose-env.yml rm -f -v' EXIT
-
-
-CONTAINER_NAME=nussknacker_kafka
-TOPIC=$1
-PARTITION_COUNT=$2
 TRANSACTION_COUNT=$((CLIENTID_COUNT * TRANSACTIONS_PER_CLIENT))
 
 calcOffsetSumInTopic() {
@@ -47,24 +17,25 @@ calcOffsetSumInTopic() {
 CLIENTID_COUNT=100
 TRANSACTIONS_PER_CLIENT=10
 TIME_DEVIATION_PARAMETER=10
-./generateBenchmarkTransactions $CLIENTID_COUNT $TRANSACTIONS_PER_CLIENT $TIME_DEVIATION_PARAMETER
+./generateBenchmarkTransactions.sh $CLIENTID_COUNT $TRANSACTIONS_PER_CLIENT $TIME_DEVIATION_PARAMETER
 
 docker exec $CONTAINER_NAME kafka-topics.sh --create --topic "$TOPIC" --zookeeper zookeeper:2181 --partitions "$PARTITION_COUNT" --replication-factor 1 --if-not-exists
 
-sumAfter=sumBefore=$(calcOffsetSumInTopic)
+sumBefore=$(calcOffsetSumInTopic)
 cat ./benchmarkTransactions.json | ./sendToKafka.sh transactions
+sumAfter=$(calcOffsetSumInTopic)
 
 waitTime=0
 sleep=10
 waitLimit=120
 offsetSum=$((sumAfter - sumBefore))
 
-while [[ $waitTime -lt $waitLimit && offsetSum -lt $TRANSACTION_COUNT]]
+while [[ $waitTime -lt $waitLimit && ($offsetSum -lt $TRANSACTION_COUNT)]]
 do
   sleep $sleep
   waitTime=$((waitTime+sleep))
   sumAfter=$(calcOffsetSumInTopic)
-  allOffsetSum=$((sumAfter-sumBefore))
+  offsetSum=$((sumAfter-sumBefore))
   if [[ offsetSum -lt $TRANSACTION_COUNT ]]
   then
     echo "All transactions have yet not been obtained by kafka within $waitTime sec, $offsetSum transactions processed so far."
