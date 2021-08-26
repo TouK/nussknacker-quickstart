4dea1881dedbc3000f4098c7cc521bef21e69c7f
apanczyk <apanczyk98@gmail.com>
2021-08-26 10:38:15 +0200

    Works on single request

--- testData/verifyEnd/specificSendTransactions.sh
+++ testData/verifyEnd/specificSendTransactions.sh
@@ -4,16 +4,15 @@ set -e
 
 cd "$(dirname $0)"
 
-RANGE=300
-for i in $(seq 1 $RANGE); do
+RANGE=$1
+for i in $(seq 1 "$RANGE"); do
   sleep 0.1
   ID=$((1 + RANDOM % 5))
   AMOUNT=$((1 + RANDOM % 30))
   NOW=$(date +%s%3N)
   TIME=$((NOW - RANDOM % 20))
   echo "{ \"clientId\": \"$ID\", \"amount\": $AMOUNT, \"eventDate\": $TIME}"
-  if [ "$i" == $RANGE ]; then
-    echo "{ \"clientId\": \"999\", \"amount\": 100, \"eventDate\": 0}"
-    echo "{ \"clientId\": \"999\", \"amount\": 100, \"eventDate\": 0}"
+  if [ "$i" == "$RANGE" ]; then
+    echo "{ \"clientId\": \"999\", \"amount\": 100, \"eventDate\": $TIME}"
   fi
 done | ./../sendToKafka.sh transactions
