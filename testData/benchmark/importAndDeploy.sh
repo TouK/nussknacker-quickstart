94b3d3ec0b95f689730c68418e0b609bd44a1c67
Damian Święcki <dswiecki9@gmail.com>
2021-09-09 12:01:14 +0200

    Externalize authorization header (#13)
    * Externalize authorization header
    
    * Review

--- testData/benchmark/importAndDeploy.sh
+++ testData/benchmark/importAndDeploy.sh
@@ -6,12 +6,15 @@ cd "$(dirname $0)"
 
 main() {
   SCHEMA=${1:-"./DetectLargeTransactionsWithFinishVerification.json"}
+  #Default authorization is basic encoded admin:admin
+  AUTHORIZATION_HEADER_VALUE=${2:-"Basic YWRtaW46YWRtaW4="}
+  AUTHORIZATION_HEADER="authorization: $AUTHORIZATION_HEADER_VALUE"
 
-  curl -X POST -u admin:admin 'http://localhost:8081/api/processManagement/cancel/DetectLargeTransactions'
-  curl -u admin:admin -X DELETE 'http://localhost:8081/api/processes/DetectLargeTransactions' -v
+  curl -X POST -H "$AUTHORIZATION_HEADER" 'http://localhost:8081/api/processManagement/cancel/DetectLargeTransactions'
+  curl -H "$AUTHORIZATION_HEADER" -X DELETE 'http://localhost:8081/api/processes/DetectLargeTransactions' -v
 
-  echo "Creating process"
-  CODE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "http://admin:admin@localhost:8081/api/processes/DetectLargeTransactions/Default?isSubprocess=false")
+  echo "Creating empty scenario $SCHEMA"
+  CODE=$(curl -s -o /dev/null -w "%{http_code}" -H "$AUTHORIZATION_HEADER" -X POST "http://localhost:8081/api/processes/DetectLargeTransactions/Default?isSubprocess=false")
   if [[ $CODE == 201 ]]; then
     echo "Scenario creation success"
   elif [[ $CODE == 400 ]]; then
@@ -22,11 +25,11 @@ main() {
     exit 1
   fi
 
-  echo "Creating required schemas for DetectLargeTransactions"
+  echo "Creating required schemas for $SCHEMA"
   ../schema/createSchemas.sh
 
-  echo "Importing scenario"
-  RESPONSE=$(curl -s -F "process=@$SCHEMA" -w "\n%{http_code}" "http://admin:admin@localhost:8081/api/processes/import/DetectLargeTransactions")
+  echo "Importing scenario $SCHEMA"
+  RESPONSE=$(curl -s -F "process=@$SCHEMA" -w "\n%{http_code}" -H "$AUTHORIZATION_HEADER" "http://localhost:8081/api/processes/import/DetectLargeTransactions")
   HTTP_CODE=$(echo "$RESPONSE" | tail -n 1)
   SCENARIO=$(echo "$RESPONSE" | sed \$d)
   if [[ "$HTTP_CODE" != 200 ]]; then
@@ -34,14 +37,14 @@ main() {
     exit 1
   fi
 
-  echo "Saving scenario"
+  echo "Saving scenario $SCHEMA"
   START='{"process":'
   END=',"comment": ""}'
-  curl -s -o /dev/null -u admin:admin -H 'Accept: application/json, text/plain, */*' -H 'Content-Type: application/json;charset=UTF-8' \
+  curl -s -o /dev/null -H "$AUTHORIZATION_HEADER" -H 'Accept: application/json, text/plain, */*' -H 'Content-Type: application/json;charset=UTF-8' \
     --data-raw "${START}${SCENARIO}${END}" -X PUT 'http://localhost:8081/api/processes/DetectLargeTransactions'
 
-  echo "Deploying scenario"
-  curl -u admin:admin -X POST 'http://localhost:8081/api/processManagement/deploy/DetectLargeTransactions' &
+  echo "Deploying scenario $SCHEMA"
+  curl -H "$AUTHORIZATION_HEADER" -X POST 'http://localhost:8081/api/processManagement/deploy/DetectLargeTransactions' &
 
   echo "Waiting for status running"
 
@@ -52,7 +55,7 @@ main() {
     sleep "$sleep"
     waitTime=$((waitTime + sleep))
 
-    STATUS=$(curl -s -u admin:admin -X GET 'http://localhost:8081/api/processes/DetectLargeTransactions/status' |
+    STATUS=$(curl -s -H "$AUTHORIZATION_HEADER" -X GET 'http://localhost:8081/api/processes/DetectLargeTransactions/status' |
       python3 -c "import sys, json; print(json.load(sys.stdin)['status']['name'])")
     if [[ $STATUS == 'RUNNING' ]]; then
       echo "Process deployed within $waitTime sec"
@@ -68,4 +71,4 @@ main() {
 }
 
 # With parameter that contains importing scheme file path
-main "$1"
+main "$1" "$2"
