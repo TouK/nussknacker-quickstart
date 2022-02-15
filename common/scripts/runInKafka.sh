a7bb4d5c215ef1d03c9a3b5ba61b8ec66bf8291d
Maciej Próchniak <mpr@touk.pl>
2022-02-15 11:56:02 +0100

    Refactor benchmarks for lite (#41)
    * Refactor benchmarks for lite
    
    * test

--- common/scripts/runInKafka.sh
+++ common/scripts/runInKafka.sh
@@ -2,7 +2,7 @@
 
 if [ -z "${RELEASE}" ]; then
   CONTAINER_NAME=$(docker ps | grep nussknacker_kafka | awk '{print $1}')
-  docker exec -i $CONTAINER_NAME $@
+  docker exec -i $CONTAINER_NAME "$@"
 else
-  kubectl exec -i $RELEASE-kafka-0 -- $@
+  kubectl exec -i $RELEASE-kafka-0 -- "$@"
 fi
