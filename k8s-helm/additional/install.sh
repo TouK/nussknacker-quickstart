21d9967b7a93df0839c7e8dd8dab1b462157d7c2
Maciek Próchniak <mpr@touk.pl>
2022-02-15 09:04:05 +0100

    Install additional components

--- k8s-helm/additional/install.sh
+++ k8s-helm/additional/install.sh
@@ -13,4 +13,4 @@ export AKHQ_HOST=localhost
 else 
 export AKHQ_HOST="$RELEASE-nussknacker.$DOMAIN"
 fi
-envsubst < akhq.yaml | helm upgrade -i $RELEASE-akhq akhq/akhq --debug --values -
+envsubst < akhq.yaml | helm upgrade -i $RELEASE-akhq akhq/akhq --debug --wait --values -
