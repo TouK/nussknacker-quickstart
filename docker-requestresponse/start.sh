fee73a107fe221d8260f9a3564484b8cddb88f7d
Maciek Próchniak <mpr@touk.pl>
2022-03-16 14:35:22 +0100

    Requestresponse

---
+++ docker-requestresponse/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname $0)"
+
+docker-compose -f docker-compose.yml -f docker-compose-env.yml -f docker-compose-custom.yml pull
+docker-compose -f docker-compose.yml -f docker-compose-env.yml -f docker-compose-custom.yml up -d
