7598fd635f7d93f970fec42f1fb0cd3d644f2ae6
Maciek Próchniak <mpr@touk.pl>
2022-03-29 18:34:34 +0200

    debug

--- common/tests/testDockerRequestResponse.sh
+++ common/tests/testDockerRequestResponse.sh
@@ -10,7 +10,7 @@ $WAIT_FOR_OK "api/processes" "Checking Nussknacker API response..." "Nussknacker
 $WAIT_FOR_OK  "metrics" "Checking Grafana response..." "Grafana not started" "grafana"
 ../scripts/importAndDeploy.sh $SCENARIO_PATH 
 $WAIT_FOR_OK  "api/processes/status" "Checking status..." "Scenario not running" "designer"
-$WAIT_FOR_OK  "api/processCounts/Loan\ request?dateFrom=2021-08-04T00:00:00%2B02:00&dateTo=2021-08-04T23:59:59%2B02:00" "Checking counts" "Counts not working" "designer"
+$WAIT_FOR_OK  "api/processCounts/Loan%20request?dateFrom=2021-08-04T00:00:00%2B02:00&dateTo=2021-08-04T23:59:59%2B02:00" "Checking counts" "Counts not working" "designer"
 
 curl -d '{customerId: "4", requestedAmount: 2000, requestType: "mortgage", location: { city: "Lublin", street: "Lipowa" }}' -f -v http://localhost:3181/scenario/loan
 
