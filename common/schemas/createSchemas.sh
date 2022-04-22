d1d5151ec44271f1cb1934091a06435c8e0abedd
Arek Burdach <arek.burdach@gmail.com>
2022-04-22 10:29:55 +0200

    Keep schemas in human readable format

--- common/schemas/createSchemas.sh
+++ common/schemas/createSchemas.sh
@@ -4,14 +4,19 @@ set -e
 
 cd "$(dirname $0)"
 
+function wrapWithSchemaRegistryMessage() {
+    NAME=$1
+    echo "{\"schema\": \"$(cat $NAME.json | sed -z -e 's/\n/\\n/g' -e 's/"/\\"/g')\"}"
+}
+
 function createSchema() {
     NAME=$1
     if [[ -z "${RELEASE}" ]]
     then
-        curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json"  --data @$NAME.json http://localhost:3082/subjects/$NAME/versions -v
+        wrapWithSchemaRegistryMessage $NAME | curl -d @- -H "Content-Type: application/vnd.schemaregistry.v1+json" http://localhost:3082/subjects/$NAME/versions -v
     else
-        cat $NAME.json | kubectl exec -i deploy/$RELEASE-apicurio-registry -- curl -d @- -H "Content-type: application/json" http://localhost:8080/apis/ccompat/v6/subjects/$NAME/versions -v
-    fi 
+        wrapWithSchemaRegistryMessage $NAME | kubectl exec -i deploy/$RELEASE-apicurio-registry -- curl -d @- -H "Content-type: application/json" http://localhost:8080/apis/ccompat/v6/subjects/$NAME/versions -v
+    fi
 }
 
 createSchema transactions-value
