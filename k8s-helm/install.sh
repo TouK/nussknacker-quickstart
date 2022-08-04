dd4ffb7981c85f406cd5a5a5dbb9a38329a13f47
Arek Burdach <arek.burdach@gmail.com>
2022-08-04 10:27:50 +0200

    added devel flag + devel flag off for main and on for staging

--- k8s-helm/install.sh
+++ k8s-helm/install.sh
@@ -8,7 +8,15 @@ helm repo add --force-update touk https://helm-charts.touk.pl/public
 helm repo add --force-update touk-snapshots https://helm-charts.touk.pl/nexus/repository/helm-snapshots/
 helm repo update
 
-HELM_REPO=${HELM_REPO:-touk/nussknacker}
+if [[ "$DEVEL" = true ]]; then
+  HELM_REPO=${HELM_REPO:-touk-snapshots/nussknacker}
+  NUSSKNACKER_VERSION=${NUSSKNACKER_VERSION:-staging-latest}
+  DEVEL_ARG="--devel"
+else
+  HELM_REPO=${HELM_REPO:-touk/nussknacker}
+  NUSSKNACKER_VERSION=${NUSSKNACKER_VERSION:-latest}
+  DEVEL_ARG=""
+fi
 
 kubectl get secret "$RELEASE-postgresql" || cat postgres-secret.yaml | POSTGRES_PASSWORD=`date +%s | sha256sum | base64 | head -c 32` RELEASE=$RELEASE MAYBE_NAMESPACE=`kubectl config view --minify -o jsonpath='{..namespace}'` NAMESPACE=${MAYBE_NAMESPACE:-default} envsubst | kubectl apply -f -
 
@@ -21,9 +29,9 @@ fi
 
 COMMAND=${COMMAND:-"upgrade -i"}
 
-helm $COMMAND "${RELEASE}" $HELM_REPO \
+helm $COMMAND $DEVEL_ARG "${RELEASE}" $HELM_REPO \
   --wait \
   $ADDITIONAL_VALS \
-  --set image.tag="${NUSSKNACKER_VERSION:-latest}" \
+  --set image.tag="${NUSSKNACKER_VERSION}" \
   --set postgresql.existingSecret="${RELEASE}-postgresql" \
   -f values.yaml $@ 
