d613932a21d1b9620d7ea4f81c5ed87860b09ae6
Damian Święcki <dswiecki9@gmail.com>
2022-08-09 10:03:47 +0200

    Changed schema format to json schema (#77)
    Co-authored-by: Arek Burdach <arek.burdach@gmail.com>

--- common/schemas/createSchemas.sh
+++ common/schemas/createSchemas.sh
@@ -10,7 +10,7 @@ cd $resolved_dir
 
 function wrapWithSchemaRegistryMessage() {
     NAME=$1
-    echo "{\"schema\": \"$(cat $NAME.json | sed -z -e 's/\n/\\n/g' -e 's/"/\\"/g')\"}"
+    echo "{\"schema\": \"$(cat $NAME.json | sed -z -e 's/\n/\\n/g' -e 's/"/\\"/g')\", \"schemaType\": \"JSON\"}"
 }
 
 function createSchema() {
