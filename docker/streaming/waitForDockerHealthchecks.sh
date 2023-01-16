3be63c829d4b969f7af19162ba2901ecae8c866c
Maciek Próchniak <mpr@touk.pl>
2023-01-16 15:02:21 +0100

    Checking docker healthchecks

---
+++ docker/streaming/waitForDockerHealthchecks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname $0)"
+export BASE_PATH=`pwd`
+export ADDITIONAL_COMPOSE_FILE="-f $(realpath ./docker-compose-streaming.yml)"
+
+SLEEP=${1-10}
+WAIT_LIMIT=${2-120}
+UNHEALTHY="x"
+
+waitTime=0
+
+while [[ $waitTime -lt $WAIT_LIMIT && "x$UNHEALTHY" != "x" ]]; do
+  waitTime=$((waitTime + $SLEEP))
+  STATUSES=`docker inspect --format='{{.Name}}:{{if .State.Health }}{{.State.Health.Status}}{{ else }}healthy{{ end }}' $(../common/invokeDocker.sh ps -q)`
+  UNHEALTHY=`echo $STATUSES | tr / '\n' | grep -v ':healthy' | tr '\n' ' '`
+  if [[ "x$UNHEALTHY" != "x" ]]; then
+    echo "Some services still not healthy: $UNHEALTHY"
+    sleep $SLEEP
+  fi
+done
+
+if [[ "x$UNHEALTHY" != "x" ]]; then
+  echo "Some services not healthy: $UNHEALTHY"
+  exit 1
+fi
+
+echo "All healthy"
