1a923acf5f54737e81e5437dbf71b4b871aff555
Mateusz Słabek <mslb@touk.pl>
2023-08-11 13:59:10 +0200

    poprawka z powodu aktualizacji Docker Compose (#146)
    Co-authored-by: lodyczekoladowe <65414380+lodyczekoladowe@users.noreply.github.com>

--- docker/common/invokeDocker.sh
+++ docker/common/invokeDocker.sh
@@ -3,4 +3,4 @@
 set -e
 
 cd "$(dirname $0)"
-docker-compose -f docker-compose.yml -f docker-compose-env.yml -f docker-compose-custom.yml --env-file=${BASE_PATH}/.env $ADDITIONAL_COMPOSE_FILE $@ 
+docker compose -f docker-compose.yml -f docker-compose-env.yml -f docker-compose-custom.yml --env-file=${BASE_PATH}/.env $ADDITIONAL_COMPOSE_FILE $@ 
