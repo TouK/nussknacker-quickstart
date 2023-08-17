322238474f27117d91e7d906930957205fb66bf0
Mateusz Słabek <mslb@touk.pl>
2023-08-17 16:16:55 +0200

    debug ci message 2

--- common/scripts/runInKafka.sh
+++ common/scripts/runInKafka.sh
@@ -3,6 +3,8 @@
 if [ -z "${RELEASE}" ]; then
   CONTAINER_NAME=$(docker ps | grep nussknacker_kafka | awk '{print $1}')
   echo "inside runInKafka -> first branch"
+  echo "$CONTAINER_NAME"
+  docker ps
   docker exec -i $CONTAINER_NAME "$@"
 else
   #In Docker-based setup we use Confluent image. In K8s setup we use Bitnami. The former has scripts without .sh suffix, while the latter - with it...
