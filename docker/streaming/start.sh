6f0b1c4b8fe0a49a67351d556be31d15c4597986
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-11-16 16:42:43 +0100

    check

--- docker/streaming/start.sh
+++ docker/streaming/start.sh
@@ -7,11 +7,11 @@ cd "$(dirname "$0")"
 # todo: remove
 # ls -la
 # echo "READLINK: $(readlink -f ./docker-compose-streaming.yml)"
-# DOCKER_COMPOSE_STREAMING_PATH=$(cd "$(dirname "./docker-compose-streaming.yml")"; pwd)/$(basename "./docker-compose-streaming.yml")
-# export ADDITIONAL_COMPOSE_FILE="-f $DOCKER_COMPOSE_STREAMING_PATH"
+DOCKER_COMPOSE_STREAMING_PATH=$(cd "$(dirname "./docker-compose-streaming.yml")"; pwd)/$(basename "./docker-compose-streaming.yml")
+export ADDITIONAL_COMPOSE_FILE="-f $DOCKER_COMPOSE_STREAMING_PATH"
 
 export BASE_PATH=$(pwd)
-export ADDITIONAL_COMPOSE_FILE="-f $(realpath ./docker-compose-streaming.yml)"
+# export ADDITIONAL_COMPOSE_FILE="-f $(realpath ./docker-compose-streaming.yml)"
 
 
 [[ "$NO_PULL" == "true" ]] || ../common/invokeDocker.sh pull
