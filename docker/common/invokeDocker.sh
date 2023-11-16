f3c658985387ccbce496c65a72380ff2ad5258c0
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-11-16 16:18:38 +0100

    check

--- docker/common/invokeDocker.sh
+++ docker/common/invokeDocker.sh
@@ -3,4 +3,8 @@
 set -ex
 
 cd "$(dirname "$0")"
+
+ls -la "/home/runner/work/nussknacker-quickstart/nussknacker-quickstart/docker/streaming"
+ls -la "/home/runner/work/nussknacker-quickstart"
+
 docker compose -f docker-compose.yml -f docker-compose-env.yml -f docker-compose-custom.yml --env-file="$BASE_PATH/.env" "$ADDITIONAL_COMPOSE_FILE" "$@" 
