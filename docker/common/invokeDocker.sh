0d5722c2a66159f49bff0b8751be13793b6fb04f
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-11-16 16:51:25 +0100

    check

--- docker/common/invokeDocker.sh
+++ docker/common/invokeDocker.sh
@@ -4,4 +4,5 @@ set -ex
 
 cd "$(dirname "$0")"
 
-docker compose -f docker-compose.yml -f docker-compose-env.yml -f docker-compose-custom.yml --env-file="$BASE_PATH/.env" $ADDITIONAL_COMPOSE_FILE "$@" 
+docker compose version
+docker compose --env-file="$BASE_PATH/.env" -f docker-compose.yml -f docker-compose-env.yml -f docker-compose-custom.yml $ADDITIONAL_COMPOSE_FILE "$@" 
