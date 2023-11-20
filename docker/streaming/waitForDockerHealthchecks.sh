ed9830ee69ac4956a15ce9294aa80eb8b38bb211
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-11-20 11:59:13 +0100

    check

--- docker/streaming/waitForDockerHealthchecks.sh
+++ docker/streaming/waitForDockerHealthchecks.sh
@@ -14,12 +14,12 @@ UNHEALTHY="x"
 waitTime=0
 
 while [[ $waitTime -lt $WAIT_LIMIT && "x$UNHEALTHY" != "x" ]]; do
-  waitTime=$((waitTime + "$SLEEP"))
+  waitTime=$((waitTime + $SLEEP))
   STATUSES=$(docker inspect --format='{{.Name}}:{{if .State.Health }}{{.State.Health.Status}}{{ else }}healthy{{ end }}' $(../common/invokeDocker.sh ps -q))
   UNHEALTHY=$(echo "$STATUSES" | sed 's/\///g' | tr ' ' '\n' | grep -v ':healthy' | tr '\n' ' ')
   if [[ "x$UNHEALTHY" != "x" ]]; then
     echo "Some services still not healthy: $UNHEALTHY"
-    sleep "$SLEEP"
+    sleep $SLEEP
   fi
 done
 
