e0a24a22023e4022bdab670bd89d6cbbb9c5667f
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-11-21 14:50:20 +0100

    check

--- common/tests/testK8sStreaming.sh
+++ common/tests/testK8sStreaming.sh
@@ -3,8 +3,8 @@
 cd "$(dirname "$0")"
 set -a; source ../../k8s-helm/.env; set +a
 
-sleep 60
-
 source ../scripts/utils.sh
+
+../common/scripts/waitForOkFromUrl.sh "api/app/healthCheck" "Checking Nussknacker Health Check API response.." "Nussknacker not started" "designer"
 
 ./testStreaming.sh "$(fullPath "../../k8s-helm/scenarios/DetectLargeTransactionsLite.json")"
