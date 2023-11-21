43e818d726530c46c231eeef91e70b89d64b143d
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-11-21 14:41:12 +0100

    check

--- common/tests/testK8sStreaming.sh
+++ common/tests/testK8sStreaming.sh
@@ -3,6 +3,8 @@
 cd "$(dirname "$0")"
 set -a; source ../../k8s-helm/.env; set +a
 
+sleep 60
+
 source ../scripts/utils.sh
 
 ./testStreaming.sh "$(fullPath "../../k8s-helm/scenarios/DetectLargeTransactionsLite.json")"
