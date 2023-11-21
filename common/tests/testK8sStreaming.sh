fe0dfd48d6929801f3166d8d16c57c0797ad4f20
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-11-21 15:19:38 +0100

    check

--- common/tests/testK8sStreaming.sh
+++ common/tests/testK8sStreaming.sh
@@ -5,7 +5,7 @@ set -a; source ../../k8s-helm/.env; set +a
 
 source ../scripts/utils.sh
 
-curl -v -u admin:admin http://localhost:8081/api/app/healthCheck"
+curl -v -u admin:admin "http://localhost:8081/api/app/healthCheck"
 
 ../scripts/waitForOkFromUrl.sh "api/app/healthCheck" "Checking Nussknacker Health Check API response.." "Nussknacker not started" "designer"
 
