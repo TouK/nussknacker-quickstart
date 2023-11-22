36e5f8fd044f095aa28abca3c7b551632d196fc1
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-11-22 10:43:01 +0100

    check

--- common/scripts/createScenarioAndDeploy.sh
+++ common/scripts/createScenarioAndDeploy.sh
@@ -26,7 +26,6 @@ deploy() {
   # todo: remove
   kubectl logs -l app.kubernetes.io/name=nussknacker --tail -1
 
-  sleep 30
   echo "Deploying scenario $SCENARIO_NAME"
   curl -Lv -H "$AUTHORIZATION_HEADER" -X POST "$DESIGNER_URL/api/processManagement/deploy/$SCENARIO_NAME"
 
