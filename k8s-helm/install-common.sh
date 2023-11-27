e0499915af2b7f5cadda1ea29a3615b297cb4c15
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-11-27 12:30:02 +0100

    check

--- k8s-helm/install-common.sh
+++ k8s-helm/install-common.sh
@@ -41,6 +41,7 @@ COMMAND=${COMMAND:-"upgrade -i"}
 helm $COMMAND $DEVEL_ARG "${RELEASE}" $HELM_REPO \
   --wait \
   $ADDITIONAL_VALS \
+  --set schemas_cache_entry_ttl="0s" \
   --set image.tag="${NUSSKNACKER_VERSION}" \
   --set nussknacker.usageStatisticsReportsFingerprint="${USAGE_REPORTS_FINGERPRINT}" \
   --set postgresql.auth.existingSecret="${RELEASE}-postgresql" $@
