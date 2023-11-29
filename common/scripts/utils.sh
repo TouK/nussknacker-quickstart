1a355b8f1c1983c03688f94ef091d26bb9b9ad42
Mateusz Kołodziejczyk <coutopl@gmail.com>
2023-11-29 15:07:54 +0100

    fixes

--- common/scripts/utils.sh
+++ common/scripts/utils.sh
@@ -4,3 +4,11 @@ fullPath() {
     local LOCATION=$1
     echo "$(cd "$(dirname "$LOCATION")"; pwd)/$(basename "$LOCATION")"
 }
+
+sha256() {
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        shasum -a 256
+    else
+        sha256sum
+    fi
+}
--- k8s-helm/install-common.sh
+++ k8s-helm/install-common.sh
@@ -27,7 +27,7 @@ else
   DEVEL_ARG=""
 fi
 
-kubectl get secret "$RELEASE-postgresql" || cat postgres-secret.yaml | POSTGRES_PASSWORD=$(date +%s | sha256sum | base64 | head -c 32) RELEASE=$RELEASE MAYBE_NAMESPACE=$(kubectl config view --minify -o jsonpath='{..namespace}') NAMESPACE=${MAYBE_NAMESPACE:-default} envsubst | kubectl apply -f -
+kubectl get secret "$RELEASE-postgresql" || cat postgres-secret.yaml | POSTGRES_PASSWORD=$(source ../common/scripts/utils.sh && date +%s | sha256 | base64 | head -c 32) RELEASE=$RELEASE MAYBE_NAMESPACE=$(kubectl config view --minify -o jsonpath='{..namespace}') NAMESPACE=${MAYBE_NAMESPACE:-default} envsubst | kubectl apply -f -
 
 if [[ -z $DOMAIN ]]
 then
@@ -41,7 +41,6 @@ COMMAND=${COMMAND:-"upgrade -i"}
 helm $COMMAND $DEVEL_ARG "${RELEASE}" $HELM_REPO \
   --wait \
   $ADDITIONAL_VALS \
-  --set schemas_cache_entry_ttl="${NUSSKNACKER_SCHEMAS_CACHE_TTL:-10s}" \
   --set image.tag="${NUSSKNACKER_VERSION}" \
   --set nussknacker.usageStatisticsReportsFingerprint="${USAGE_REPORTS_FINGERPRINT}" \
   --set postgresql.auth.existingSecret="${RELEASE}-postgresql" \
