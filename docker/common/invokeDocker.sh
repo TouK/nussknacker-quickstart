32af7dbf6c77104e01a7afccc02382c9a1520ac6
Łukasz Ciołecki <lukasz.ciolecki@gmail.com>
2024-03-06 21:08:48 +0100

    Fix: Docker Componse Command (#161)

--- docker/common/invokeDocker.sh
+++ docker/common/invokeDocker.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -e
 
-if ! command -v "docker-compose" &> /dev/null; then
-    echo "docker-compose does not exist. Please install it first https://docs.docker.com/compose/install/"
-    exit 1
+DOCKER_COMPOSE_COMMAND=""
+if command -v docker-compose &> /dev/null; then
+    DOCKER_COMPOSE_COMMAND="docker-compose"
+elif command -v docker compose &> /dev/null; then
+    DOCKER_COMPOSE_COMMAND="docker compose"
+else
+  echo "Docker Compose does not exist. Please install it first https://docs.docker.com/compose/install/"
+  exit 1
 fi
 
 cd "$(dirname "$0")"
 
-docker-compose --env-file "$BASE_PATH/.env" -f docker-compose.yml -f docker-compose-env.yml -f docker-compose-custom.yml $ADDITIONAL_COMPOSE_FILE "$@" 
+$DOCKER_COMPOSE_COMMAND --env-file "$BASE_PATH/.env" -f docker-compose.yml -f docker-compose-env.yml -f docker-compose-custom.yml $ADDITIONAL_COMPOSE_FILE "$@"
