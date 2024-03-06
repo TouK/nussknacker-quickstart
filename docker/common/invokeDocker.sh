f5520d251266a9fe53c9839a871fad5af60a5afc
Łukasz Ciołecki <lcl@ukaszs-mbp.home>
2024-03-06 22:29:28 +0100

    Like gtp has said..

--- docker/common/invokeDocker.sh
+++ docker/common/invokeDocker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 DOCKER_COMPOSE_COMMAND=""
-if command -v docker &>/dev/null && docker compose version &>/dev/null; then
-    DOCKER_COMPOSE_COMMAND="docker compose"
-elif command -v docker-compose &> /dev/null; then
-    DOCKER_COMPOSE_COMMAND="docker-compose"
+if docker compose version &> /dev/null; then
+  DOCKER_COMPOSE_COMMAND="docker compose"
+elif docker-compose version &> /dev/null; then
+  DOCKER_COMPOSE_COMMAND="docker-compose"
 else
   echo "Docker Compose does not exist. Please install it first https://docs.docker.com/compose/install/"
   exit 1
