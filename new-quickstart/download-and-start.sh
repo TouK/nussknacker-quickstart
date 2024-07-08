b4ae1d60776d55baace8c18912a41428d88c5f54
Mateusz Kołodziejczyk <coutopl@gmail.com>
2024-07-08 19:12:44 +0200

    improvements

--- new-quickstart/download-and-start.sh
+++ new-quickstart/download-and-start.sh
@@ -4,7 +4,7 @@ set -e
 BRANCH_NAME="NU-1726"
 ZIP_NAME="$BRANCH_NAME.zip"
 
-wget "https://github.com/TouK/nussknacker-quickstart/archive/refs/heads/feature/$ZIP_NAME"
+wget 'https://github.com/TouK/nussknacker-quickstart/archive/refs/heads/feature/'$ZIP_NAME
 rm -rf "$BRANCH_NAME"
 unzip "$ZIP_NAME"    
 cd "$BRANCH_NAME"
