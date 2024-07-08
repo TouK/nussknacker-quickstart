14bdf89ab8a3aa598866153806bc8d8e7d60509b
Mateusz Kołodziejczyk <coutopl@gmail.com>
2024-07-08 19:09:46 +0200

    improvements

---
+++ new-quickstart/download-and-start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+BRANCH_NAME="NU-1726"
+ZIP_NAME="$BRANCH_NAME.zip"
+
+wget "https://github.com/TouK/nussknacker-quickstart/archive/refs/heads/feature/$ZIP_NAME"
+unzip "$ZIP_NAME"    
+cd $BRANCH_NAME
+./start.sh
