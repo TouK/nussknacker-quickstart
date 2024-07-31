8dcce99af4c4dc501234c3cdb22b2bb5c490346c
Mateusz Kołodziejczyk <coutopl@gmail.com>
2024-07-31 12:10:38 +0200

    wip

--- mocks/http-service/scripts/run-wiremock.sh
+++ mocks/http-service/scripts/run-wiremock.sh
@@ -2,6 +2,7 @@
 
 java $JAVA_OPTS -cp /var/wiremock/lib/*:/var/wiremock/extensions/* wiremock.Run \
     --port=8080 \
+    --root-dir=/home/wiremock \
     --max-request-journal=1000 \
     --local-response-templating \
     --async-response-enable=true \
