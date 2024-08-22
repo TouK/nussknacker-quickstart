63c5607219e97bbf069b839ff10e76c974f43d20
Mateusz Kołodziejczyk <coutopl@gmail.com>
2024-08-22 11:33:35 +0200

    wip

--- quickstart-setup/setup/run-setup.sh
+++ quickstart-setup/setup/run-setup.sh
@@ -4,4 +4,5 @@ cd "$(dirname "$0")"
 
 ./schema-registry/setup-schemas.sh
 ./kafka/setup-topics.sh
-./nu/import-and-deploy-example-scenarios.sh
+#./nu/
+# ./nu/import-and-deploy-example-scenarios.sh
